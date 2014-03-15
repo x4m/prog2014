723f7a1e1ff20bd64e9994884f215ac0c8391416
Andrey Borodin <amborodin@acm.org>
2014-03-15 15:42:27 +0600

    Материалы лекции по цифровой обработке сигнала

---
+++ PhotoProcesser/PhotoProcesser/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PhotoProcesser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(107, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(831, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(107, 376);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(831, 340);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "do";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 115);
+            this.trackBar1.Maximum = 200;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(69, 586);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 728);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ PhotoProcesser/PhotoProcesser/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleImageProcessing;
+
+namespace PhotoProcesser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap origin;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog(){Filter = "*.jpg|*.jpg"};
+            if (ofd.ShowDialog(this) == DialogResult.OK)
+            {
+                origin = (Bitmap) Image.FromFile(ofd.FileName);
+                pictureBox1.Image = origin;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var src = new ImagerBitmap(origin.Clone() as Bitmap);
+            var dst = new ImagerBitmap(origin.Clone() as Bitmap);
+            
+            for (int x = 1; x < src.Bitmap.Width-1; x++)
+                for (int y = 1; y < src.Bitmap.Height-1; y++)
+                {
+                    Color c = src.GetPixel(x, y);
+                    Color c1 = src.GetPixel(x-1, y);
+                    Color c2 = src.GetPixel(x+1, y);
+
+                    Color c3 = src.GetPixel(x-1, y+1);
+                    Color c4 = src.GetPixel(x+1, y+1);
+                    Color c5 = src.GetPixel(x-1, y-1);
+                    Color c6 = src.GetPixel(x+1, y-1);
+
+                    Color c7 = src.GetPixel(x, y+1);
+                    Color c8 = src.GetPixel(x, y-1);
+
+                    List<Color> colors = new List<Color>(){c,c1,c2,c3,c4,c5,c6,c7,c8};
+                    colors.Sort(CompareColors);
+
+                    var resultColor = colors[colors.Count/2];
+                    dst.SetPixel(x,y,resultColor);
+                }
+
+            src.UnlockBitmap();
+            dst.UnlockBitmap();
+
+            pictureBox2.Image = dst.Bitmap;
+        }
+
+        private int CompareColors(Color c, Color c1)
+        {
+            double grey = (c.R * c.R + c.G * c.G + c.B * c.B) ;
+            double grey1 = (c1.R * c1.R + c1.G * c1.G + c1.B * c1.B) ;
+            return (int) (grey - grey1);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            var src = new ImagerBitmap(origin.Clone() as Bitmap);
+            var dst = new ImagerBitmap(origin.Clone() as Bitmap);
+            int treshold = trackBar1.Value;
+            for (int x = 0; x < src.Bitmap.Width; x++)
+                for (int y = 0; y < src.Bitmap.Height; y++)
+                {
+                    Color c = src.GetPixel(x, y);
+                    double grey = Math.Sqrt(c.R * c.R + c.G * c.G + c.B * c.B) / Math.Sqrt(3);
+                    double distance =
+                        Math.Sqrt((c.R - grey) * (c.R - grey) + (c.G - grey) * (c.G - grey) + (c.B - grey) * (c.B - grey));
+                    
+                    if (distance < treshold)
+                    {
+                        dst.SetPixel(x, y, Color.FromArgb(c.G, c.B, c.R));
+                    }
+                    else
+                    {
+                        dst.SetPixel(x, y, c);
+                    }
+                }
+            src.UnlockBitmap();
+            dst.UnlockBitmap();
+
+            pictureBox2.Image = dst.Bitmap;
+        }
+    }
+}
